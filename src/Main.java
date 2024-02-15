37b8b8c202942c1c1d28374482c0e216aa6a264a
Filip Roček <rocek.filip@szenam.cz>
2024-02-15 21:24:47 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+   public static void main(String[] args) {
+      System.out.println("Prodejci");
+      String jmeno = "Pavel Novák";
+      String datumNarozeni = "1.9.1999";
+      int pocetSjednanychSmluv = 10;
+      String mesto = "Praha";
+      String spz = "4AU 7589";
+      String ipAddress = "192.168.48.39";
+   }
+         public static void classic(String[]for (String arg : args) {
+         int weight = 1000;
+         String unit = "kg";
+         String vegetable = "mrkev"
+      }
+      public class FuelConsumption {
+         public static void main(String[] args) {
+            double distance = 100;
+            double fuelConsumption = 5;
+
+            double totalFuelConsumed = distance * fuelConsumption;
+
+            System.out.println("Total fuel consumed: " + totalFuelConsumed + " liters");
+         }
+      }
+}
